215a2e4ff637a22841c0fdba5ed5374c6b7804af
Ernestico <developer1575@gmail.com>
2024-07-13 15:02:33 -0400

    Deploy 2

--- server/server.cjs
+++ server/server.cjs
@@ -8,8 +8,8 @@ const port =  3000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.use("/public",express.static(path.join(__dirname, '../dist')));
-console.log(path.join(__dirname, '../dist'))
+app.use("/public",express.static(path.join(__dirname, '/dist')));
+console.log()
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}`);
