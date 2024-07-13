20cd16ffff274f0f70d1f25d68786d13101ca766
Ernestico <developer1575@gmail.com>
2024-07-13 15:25:57 -0400

    Deploy 2

--- server/server.cjs
+++ server/server.cjs
@@ -8,7 +8,7 @@ const port =  3000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.use("/public",express.static(path.join(__dirname, '/dist')));
+app.use("/public",express.static(path.join(__dirname, '/public')));
 console.log()
 
 app.listen(port, () => {
