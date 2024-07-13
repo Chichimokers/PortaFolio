f33ed03c5c3ffd487ea515baab92a25037986523
Ernestico <developer1575@gmail.com>
2024-07-13 14:41:55 -0400

    Deploy 1

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port =  3000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
-app.use("/public",express.static(path.join(__dirname, '../dist')));
+app.use("public",express.static(path.join(__dirname, '../dist')));
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}`);
 });
